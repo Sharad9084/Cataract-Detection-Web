b648f3de57c80e4e1071672c8ff1411a4aceea8d
Sharad9084 <155624686+Sharad9084@users.noreply.github.com>
2026-01-30 09:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,5 +213,8 @@ def prediction():
 # ------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
